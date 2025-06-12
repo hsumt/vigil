2cad411a4c4823100bfaf27c60f0c07325a39a9b
hsumt <matthew.t.hsu@gmail.com>
2025-06-12 16:46:10 -0700

    minor changes

--- src/performance_graphs.py
+++ src/performance_graphs.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 def plot_team_performance(team_df, team_number):
-    
+
     df = team_df.sort_values(by='match')
     matches = df['match']
     auto_points = df['autoPoints']
@@ -11,5 +11,11 @@ def plot_team_performance(team_df, team_number):
 
     plt.plot(matches, auto_points, marker='o', label='Auto Points', color="blue")
     plt.plot(matches, teleop_points, marker='o', label='Teleop Points', color="green")
+
+    plt.title(f"Team {team_number}'s Performance Across Recorded matches")
+    plt.legend(loc='upper left', fontsize = 'small')
+    plt.xlabel('Match Labels')
+    plt.ylabel('Points (Auto/Tele, etc.)')
+    plt.xticks(rotation = 45)
 
     plt.show()
