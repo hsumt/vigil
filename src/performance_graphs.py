e81f86ac7756117bd34316c72d794f517aea028b
hsumt <matthew.t.hsu@gmail.com>
2025-06-16 17:22:46 -0700

    Issue resolved with graph function not returning numerically based off match

--- src/performance_graphs.py
+++ src/performance_graphs.py
@@ -1,8 +1,8 @@
 import matplotlib.pyplot as plt
 import numpy as np
 def plot_team_performance(team_df, team_number):
-
-    df = team_df.sort_values(by='match')
+    team_df['match_num'] = team_df['match'].str.extract(r'(\d+)').astype(int)
+    df = team_df.sort_values(by='match_num')
     matches = df['match']
     auto_points = df['autoPoints']
     teleop_points = df['teleopPoints']
